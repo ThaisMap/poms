d23aef1d05bed4466154c77b8f5db061511a6c75
Thais <thais.marpi@gmail.com>
2021-07-20 19:28:24 -0300

    feat: adiciona estratégia de internacionalização
    - instala as bibliotecas i18next e react-i8next
    - faz a configuração inicial do i8next
    - adiciona 2 arquivos de traduções (pt e en)
    - faz uso dos textos traduzidos na tela de timer e nos menus do navigator
    - corrige um typo

---
+++ src/i18n/translations/en.ts
@@ -0,0 +1,14 @@
+const en = {
+  translation: {
+    pomodoro: 'Pomodoro',
+    shortBreak: 'Short Break',
+    longBreak: 'Long Break',
+    nav: {
+      timer: 'Timer',
+      report: 'Report',
+      break: 'Breaks',
+      config: 'Config',
+    },
+  },
+};
+export default en;
